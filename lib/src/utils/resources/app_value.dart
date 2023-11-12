7f089ccb9746889086d970880d36abf10167ebe1
ravshan1119 <allamurodovravshan38@gmail.com>
2023-11-12 22:06:43 +0500

    update

---
+++ lib/src/utils/resources/app_value.dart
@@ -0,0 +1,82 @@
+class AppMargin {
+  static const double m0 = 0.0;
+  static const double m8 = 8.0;
+  static const double m6 = 6.0;
+  static const double m10 = 10.0;
+  static const double m12 = 12.0;
+  static const double m14 = 14.0;
+  static const double m16 = 16.0;
+  static const double m18 = 18.0;
+  static const double m20 = 20.0;
+  static const double m25 = 25.0;
+}
+
+class AppPadding {
+  static const double p2 = 2.0;
+  static const double p4 = 4.0;
+  static const double p6 = 6.0;
+  static const double p8 = 8.0;
+  static const double p12 = 12.0;
+  static const double p15 = 15.0;
+  static const double p10 = 10.0;
+  static const double p14 = 14.0;
+  static const double p16 = 16.0;
+  static const double p18 = 18.0;
+  static const double p20 = 20.0;
+  static const double p24 = 24.0;
+  static const double p25 = 25.0;
+  static const double p32 = 32.0;
+  static const double p36 = 36.0;
+}
+
+class AppSize {
+  static const double s0 = 0.0;
+  static const double s1 = 1.0;
+  static const double s3 = 3.0;
+  static const double s4 = 4.0;
+  static const double s5 = 5.0;
+  static const double s6 = 6.0;
+  static const double s8 = 8.0;
+  static const double s10 = 10.0;
+  static const double s12 = 12.0;
+  static const double s13 = 13.0;
+  static const double s14 = 14.0;
+  static const double s15 = 15.0;
+  static const double s16 = 16.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+  static const double s22 = 22.0;
+  static const double s24 = 24.0;
+  static const double s25 = 25.0;
+  static const double s28 = 28.0;
+  static const double s30 = 30.0;
+  static const double s36 = 36.0;
+  static const double s40 = 40.0;
+  static const double s45 = 45.0;
+  static const double s48 = 48.0;
+  static const double s50 = 50.0;
+  static const double s52 = 52.0;
+  static const double s60 = 60.0;
+  static const double s70 = 70.0;
+  static const double s73 = 73.0;
+  static const double s84 = 84.0;
+  static const double s100 = 100.0;
+  static const double s110 = 110.0;
+  static const double s115 = 115.0;
+  static const double s120 = 120.0;
+  static const double s130 = 130.0;
+  static const double s140 = 140.0;
+  static const double s150 = 150.0;
+  static const double s160 = 160.0;
+  static const double s175 = 175.0;
+  static const double s180 = 180.0;
+  static const double s184 = 184.0;
+  static const double s190 = 190.0;
+  static const double s200 = 200.0;
+  static const double s220 = 220.0;
+  static const double s230 = 230.0;
+  static const double s240 = 240.0;
+  static const double s370 = 370.0;
+  static const double s400 = 400.0;
+  static const double s800 = 800.0;
+}
